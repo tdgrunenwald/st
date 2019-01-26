08a577a293a0f3253fabb73478eb149dd1320f77
Tyler Grunenwald <tylergrunenwald@gmail.com>
2019-01-25 20:06:16 -0600

    Revert "changed font to a FiraCode Nerd Font"
    This reverts commit 81cd5d05a21136504967d859d55f8d810720faf7.

--- config.def.h
+++ config.def.h
@@ -4,7 +4,8 @@
  * appearance
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
- */ static char *font = "FuraCode Nerd Font:pixelsize=12:antialias=true:autohint=true";
+ */
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
