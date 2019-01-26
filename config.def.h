81cd5d05a21136504967d859d55f8d810720faf7
Tyler Grunenwald <tylergrunenwald@gmail.com>
2019-01-25 19:56:11 -0600

    changed font to a FiraCode Nerd Font

--- config.def.h
+++ config.def.h
@@ -4,8 +4,7 @@
  * appearance
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
- */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+ */ static char *font = "FuraCode Nerd Font:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
